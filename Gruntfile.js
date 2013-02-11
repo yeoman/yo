32d75d2209766e3abaee1d0c1a278b1caf85750d
Sindre Sorhus <sindresorhus@gmail.com>
2013-02-11 14:33:06 +0100

    Init
    Moved from https://github.com/yeoman/yeoman

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+var util = require('util');
+var path = require('path');
+
+module.exports = function( grunt ) {
+  'use strict';
+
+  grunt.initConfig({
+    lint: {
+      options: {
+        options: '<json:.jshintrc>',
+        global: {
+          process: true
+        }
+      },
+      grunt: [
+        'Gruntfile.js',
+        //'tasks/*.js',
+      ],
+      lib: [
+        //'lib/{plugins,utils}/*.js',
+        //'lib/generators/*.js'
+      ],
+      test: [
+        //'test/**/*.js'
+      ]
+    },
+    watch: {
+      files: '<config:lint>',
+      tasks: 'lint'
+    }
+  });
+
+  // Disable lint for now until we upgrade to latest grunt with latest jshint
+  grunt.registerTask( 'default', 'lint' );
+};
