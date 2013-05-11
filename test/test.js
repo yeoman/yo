6916b898fcb14a4f12ce1d50626e73002639ebb0
Sindre Sorhus <sindresorhus@gmail.com>
2013-05-11 11:54:12 +0200

    Fix tests
    Fixes #1

--- test/test.js
+++ test/test.js
@@ -1,37 +1,36 @@
 /*global describe, it */
 'use strict';
-
 var fs = require('fs');
 var path = require('path');
-var spawn = require('child_process').spawn;
+var execFile = require('child_process').execFile;
 var assert = require('assert');
 var pkg = require('../package.json');
 var eol = require('os').EOL;
 
 describe('bin', function () {
   it('should exit with status 0 if there were no errors', function (cb) {
-    var bin = spawn('node', [path.join(__dirname, '../', pkg.bin.yo)]);
+    var cp = execFile('node', [path.join(__dirname, '../', pkg.bin.yo)]);
 
-    bin.on('exit', function (code) {
+    cp.on('exit', function (code) {
       assert.equal(code, 0);
       cb();
     });
   });
 
   it('should exit with status 1 if there were errors', function (cb) {
-    var bin = spawn('node', [path.join(__dirname, '../', pkg.bin.yo), 'notexisting']);
+    var cp = execFile('node', [path.join(__dirname, '../', pkg.bin.yo), 'notexisting']);
 
-    bin.on('exit', function (code) {
+    cp.on('exit', function (code) {
       assert.equal(code, 1);
       cb();
     });
   });
 
   it('should return the version', function (cb) {
-    var bin = spawn('node', [path.join(__dirname, '../', pkg.bin.yo), '--version']);
+    var cp = execFile('node', [path.join(__dirname, '../', pkg.bin.yo), '--version']);
     var expected = pkg.version + eol;
 
-    bin.stdout.on('data', function (data) {
+    cp.stdout.on('data', function (data) {
       assert.equal(data, expected);
       cb();
     });
