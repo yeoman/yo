daa4831620dd45b26c21035d89d56f31c606e9b2
Simon Boudrias <admin@simonboudrias.com>
2014-11-25 23:41:11 -0800

    Extract home route from yoyo

--- yoyo.js
+++ yoyo.js
@@ -1,27 +1,8 @@
-/* jshint onevar:false */
 'use strict';
-var async = require('async');
-var opn = require('opn');
-var gen = require('yeoman-generator');
-var yosay = require('yosay');
-var util = require('util');
-var path = require('path');
-var fs = require('fs');
-var updateNotifier = require('update-notifier');
-var chalk = require('chalk');
-var fullname = require('fullname');
-var namespaceToName = require('./lib/utils').namespaceToName;
-var globalConfigHasContent = require('./lib/utils/global-config').hasContent;
-var _s = require('underscore.string');
-var Configstore = require('configstore');
-var pkg = require('./package.json');
 var Router = require('./lib/router');
-var conf = new Configstore(pkg.name, {
-  generatorRunCount: {}
-});
 
 function initRouter(generator) {
-  var router = new Router(generator.env, generator.insight, conf);
+  var router = new Router(generator.env, generator.insight);
   router.insight.track('yoyo', 'init');
   router.registerRoute('help', require('./lib/routes/help'));
   router.registerRoute('update', require('./lib/routes/update'));
@@ -29,9 +10,7 @@ function initRouter(generator) {
   router.registerRoute('install', require('./lib/routes/install'));
   router.registerRoute('exit', require('./lib/routes/exit'));
   router.registerRoute('clearConfig', require('./lib/routes/clear-config'));
-  router.registerRoute('home', function () {
-    generator.home();
-  });
+  router.registerRoute('home', require('./lib/routes/home'));
 
   process.once('exit', router.navigate.bind(router, 'exit'));
 
@@ -39,162 +18,9 @@ function initRouter(generator) {
 }
 
 // The `yo yo` generator provides users with a few common, helpful commands.
-var yoyo = module.exports = function (args, options) {
-  gen.Base.apply(this, arguments);
-  this.insight = options.insight;
+var yoyo = module.exports = function () {
+  this.insight = this.options.insight;
   this.router = initRouter(this);
-};
-
-util.inherits(yoyo, gen.Base);
-
-// Prompts the user to select which generators to update
-yoyo.prototype._promptToUpdateGenerators = function () {
-  this.router.navigate('update');
-};
-
-// Initializes a generator.
-//
-// - name - (string) The generator to initialize.
-yoyo.prototype._initGenerator = function (name, done) {
-  this.router.navigate('run', name);
-};
-
-
-// Serves as the response prompt for "Install a generator" as well as simply
-// installs a generator if a string is passed in.
-//
-// - pkgName - (optional) A string that matches the NPM package name.
-yoyo.prototype._installGenerator = function (pkgName) {
-  return this.router.navigate('install');
-};
-
-// Prompts user with a few helpful resources, then opens it in their browser.
-yoyo.prototype._findHelp = function () {
-  this.router.navigate('help');
-};
-
-// I'm sorry...
-yoyo.prototype._noop = function () {};
-
-
-// Rolls through all of the generators provided by `env.generators`, finding
-// their `package.json` files, then storing them internally in `this.pkgs`.
-yoyo.prototype.findGenerators = function () {
   this.router.updateAvailableGenerators();
-  this.pkgs = this.router.generators;
-};
-
-
-// Display the home `yo` screen, with the intial set of options.
-//
-// - options - (optional)
-//           - message (string) - String to print before prompt.
-//           - refresh (bool) - Spawn a new `yo` command.
-yoyo.prototype.home = function (options) {
-  var done = this.async();
-  this.pkgs = this.router.generators;
-
-  options = options || {};
-
-  if (options.refresh) {
-    this.env.lookup(function () {
-      this.findGenerators();
-      this.home({ message: options.message });
-    }.bind(this));
-    return;
-  }
-
-  if (options.message) {
-    console.log('\n' + chalk.cyan(options.message) + '\n');
-  }
-
-  var defaultChoices = [{
-    name: 'Install a generator',
-    value: {
-      method: '_installGenerator'
-    }
-  }, {
-    name: 'Find some help',
-    value: {
-      method: '_findHelp'
-    }
-  }, {
-    name: 'Get me out of here!',
-    value: {
-      method: '_noop'
-    }
-  }];
-
-  if (globalConfigHasContent()) {
-    defaultChoices.splice(defaultChoices.length - 1, 0, {
-      name: 'Clear global config',
-      value: {
-        method: '_clearGlobalConfig'
-      }
-    });
-  }
-
-  var generatorList = this._.chain(this.pkgs).map(function (generator) {
-    if (!generator.appGenerator) {
-      return;
-    }
-
-    var updateInfo = generator.updateAvailable ? chalk.dim.yellow(' ♥ Update Available!') : '';
-
-    // TODO
-    // var updateInfo = generator.updateAvailable ?
-    //   chalk.dim.yellow(' ♥ Update Available!') +
-    //   chalk.dim.reset(' (Press Space to update)') : '';
-
-    return {
-      name: generator.prettyName + updateInfo,
-      value: {
-        method: '_initGenerator',
-        args: generator.namespace
-      }
-    };
-  }).compact().sortBy(function (el) {
-    var generatorName = namespaceToName(el.value.args);
-    return -conf.get('generatorRunCount')[generatorName] || 0;
-  }).value();
-
-  if (generatorList.length) {
-    defaultChoices.unshift({
-      name: 'Update your generators',
-      value: {
-        method: '_promptToUpdateGenerators'
-      }
-    });
-  }
-
-  this.insight.track('yoyo', 'home');
-
-  fullname(function (err, name) {
-    if (err) {
-      done(err);
-      return;
-    }
-
-    var allo = name ? ('\'Allo ' + name.split(' ')[0] + '! ') : '\'Allo! ';
-
-    this.prompt([{
-      name: 'whatNext',
-      type: 'list',
-      message: allo + 'What would you like to do?',
-      choices: this._.flatten([
-        new gen.inquirer.Separator('Run a generator'),
-        generatorList,
-        new gen.inquirer.Separator(),
-        defaultChoices,
-        new gen.inquirer.Separator()
-      ])
-    }], function (answer) {
-      this[answer.whatNext.method](answer.whatNext.args, done);
-    }.bind(this));
-  }.bind(this));
-};
-
-// Prompts the user to select which generators to clear
-yoyo.prototype._clearGlobalConfig = function () {
-  this.router.navigate('clearConfig');
+  this.router.navigate('home');
 };
