2f8cacff4189a0b3cd2c5adeebf95125ec6b1ce1
Mickael Daniel <daniel.mickael@gmail.com>
2016-05-06 08:57:58 +0200

    completion: rm comments

--- test/completion.js
+++ test/completion.js
@@ -118,9 +118,6 @@ describe('Completion', function () {
     });
 
     describe('#complete', function () {
-      // SKipping on CI right now, otherwise might introduce an `npm install
-      // generator-dummy` as a pretest script
-
       before(function (done) {
         execFile('npm', ['install', 'generator-dummy', '-g'], done);
       });
