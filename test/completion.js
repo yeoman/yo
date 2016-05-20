260b73f522f66d1c16dcc853913bdc72e8e232d9
Mickael Daniel <daniel.mickael@gmail.com>
2016-05-20 02:21:39 +0200

    completion: fix zsh / bash, implement generator / flags description
    Implement generator / flags description
    
    completion: remove help output parsing, too slow / unreliable , will probably try to figure out options / arguments from generator
    instead.
    
    review: use babel default plugin export
    
    review: update help for completion command
    
    completion: npm hooks to install / uninstall tabtab, silently
    
    completion: update tabtab version

--- test/completion.js
+++ test/completion.js
@@ -1,29 +1,10 @@
 'use strict';
-
-var path = require('path');
 var assert = require('assert');
 var events = require('events');
 var execFile = require('child_process').execFile;
 var Completer = require('../lib/completion/completer');
 var completion = require('../lib/completion');
 var find = require('lodash').find;
-
-var help = [
-  '  Usage:',
-  '  yo backbone:app [options] [<app_name>]',
-  '',
-  '  Options:',
-  '    -h,   --help                # Print the generator\'s options and usage',
-  '          --skip-cache          # Do not remember prompt answers                         Default: false',
-  '          --skip-install        # Do not automatically install dependencies              Default: false',
-  '          --appPath             # Name of application directory                          Default: app',
-  '          --requirejs           # Support requirejs                                      Default: false',
-  '          --template-framework  # Choose template framework. lodash/handlebars/mustache  Default: lodash',
-  '          --test-framework      # Choose test framework. mocha/jasmine                   Default: mocha',
-  '',
-  '  Arguments:',
-  '    app_name    Type: String  Required: false'
-].join('\n');
 
 describe('Completion', function () {
 
@@ -33,17 +14,16 @@ describe('Completion', function () {
 
   describe('Test completion STDOUT output', function () {
     it('Returns the completion candidates for both options and installed generators', function (done) {
-      var yocomplete = path.join(__dirname, '../lib/completion/index.js');
-      var yo = path.join(__dirname, '../lib/cli');
+      var cmd = 'DEBUG="tabtab*" SHELL=zsh COMP_POINT="4" COMP_LINE="yo" COMP_CWORD="yo" ';
+      cmd += 'node lib/completion/index.js completion -- yo';
 
-      var cmd = 'export cmd=\"yo\" && DEBUG=\"tabtab*\" COMP_POINT=\"4\" COMP_LINE=\"$cmd\" COMP_CWORD=\"$cmd\"';
-      cmd += 'node ' + yocomplete + ' completion -- ' + yo + ' $cmd';
-
+      console.log('cmd', cmd);
       execFile('bash', ['-c', cmd], function (err, out) {
         if (err) {
           return done(err);
         }
 
+        console.log('out:', out);
         assert.ok(/-f/.test(out));
         assert.ok(/--force/.test(out));
         assert.ok(/--version/.test(out));
@@ -69,7 +49,7 @@ describe('Completion', function () {
       // instance directly to completer.
       this.getGeneratorsMeta = this.env.getGeneratorsMeta;
 
-      this.env.getGeneratorsMeta =  function () {
+      this.env.getGeneratorsMeta = function () {
         return {
           'dummy:app': {
             resolved: '/home/user/.nvm/versions/node/v6.1.0/lib/node_modules/generator-dummy/app/index.js',
@@ -87,19 +67,6 @@ describe('Completion', function () {
 
     afterEach(function () {
       this.env.getGeneratorsMeta = this.getGeneratorsMeta;
-    });
-
-    describe('#parseHelp', function () {
-      it('Returns completion items based on help output', function () {
-        var results = this.completer.parseHelp('backbone:app', help);
-        var first = results[0];
-
-        assert.equal(results.length, 6);
-        assert.deepEqual(first, {
-          name: '--skip-cache',
-          description: 'Do not remember prompt answers                         Default-> false'
-        });
-      });
     });
 
     describe('#item', function () {
@@ -126,27 +93,6 @@ describe('Completion', function () {
       });
     });
 
-    describe('#generator', function () {
-      it('Returns completion candidates from generator help output', function (done) {
-        // Here we test against yo --help (could use dummy:yo --help)
-        this.completer.complete({ last: '' }, function (err, results) {
-          if (err) {
-            return done(err);
-          }
-
-          /* eslint no-multi-spaces: 0 */
-          assert.deepEqual(results, [
-            { name: '--force',    description: 'Overwrite files that already exist' },
-            { name: '--version',  description: 'Print version' },
-            { name: '--no-color', description: 'Disable colors' },
-            { name: '-f',         description: 'Overwrite files that already exist' }
-          ]);
-
-          done();
-        });
-      });
-    });
-
     describe('#complete', function () {
       it('Returns the list of user installed generators as completion candidates', function (done) {
         this.completer.complete({ last: 'yo' }, function (err, results) {
@@ -159,7 +105,7 @@ describe('Completion', function () {
           });
 
           assert.equal(dummy.name, 'dummy:yo');
-          assert.equal(dummy.description, 'yo');
+          assert.equal(dummy.description, 'yo from dummy generator');
 
           done();
         });
