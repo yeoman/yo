82a37a41c41e21459f475886b931e5a8a54ce07f
Maximilian Lenkeit <mlenkeit@users.noreply.github.com>
2016-05-31 20:01:56 +0200

    Fix main generator detection pattern (#447)
    * add helper to create fake read-pkg-up module
    
    * cover Router#updateAvailableGenerators with tests
    
    * ignore sub-generators that contain 'app'
    
    * inline fake read-pkg-up module
    
    * Revert "add helper to create fake read-pkg-up module"
    
    This reverts commit a6f7ff7823f404fb98ed957037066ddff151173c.

--- test/router.js
+++ test/router.js
@@ -2,11 +2,31 @@
 var assert = require('assert');
 var _ = require('lodash');
 var sinon = require('sinon');
-var Router = require('../lib/router');
+var helpers = require('./helpers');
+var proxyquire = require('proxyquire');
+var path = require('path');
+var Router = proxyquire('../lib/router', {
+  'read-pkg-up': {
+    sync: function (options) {
+      // turn /phoenix/app into phoenix-app
+      var name = options.cwd.split(path.sep).filter(function (chunk) {
+        return !!chunk;
+      }).join('-');
+      return {
+        pkg: {
+          name: name,
+          version: '0.1.0'
+        }
+      };
+    }
+  }
+});
 
 describe('Router', function () {
   beforeEach(function () {
-    this.router = new Router(sinon.stub());
+    this.env = helpers.fakeEnv();
+    this.env.getGeneratorsMeta = sinon.stub();
+    this.router = new Router(this.env);
   });
 
   describe('#registerRoute()', function () {
@@ -35,6 +55,45 @@ describe('Router', function () {
 
     it('throws on invalid route name', function () {
       assert.throws(this.router.navigate.bind(this.route, 'invalid route name'));
+    });
+  });
+
+  describe('#updateAvailableGenerators()', function () {
+    beforeEach(function () {
+      this.env.getGeneratorsMeta.returns([
+        {
+          namespace: 'xanadu:all',
+          resolved: '/xanadu/all/index.js'
+        },
+        {
+          namespace: 'phoenix:app',
+          resolved: '/phoenix/app/index.js'
+        },
+        {
+          namespace: 'phoenix:misc',
+          resolved: '/phoenix/misc/index.js'
+        },
+        {
+          namespace: 'phoenix:sub-app',
+          resolved: '/phoenix/sub-app/index.js'
+        }
+      ]);
+    });
+
+    it('finds generators where an `all` generator is implemented', function () {
+      this.router.updateAvailableGenerators();
+      assert.ok(this.router.generators['xanadu-all'], 'xanadu:all found');
+    });
+
+    it('finds generators where an `app` generator is implemented', function () {
+      this.router.updateAvailableGenerators();
+      assert.ok(this.router.generators['phoenix-app'], 'phoenix:app found');
+    });
+
+    it('ignores sub-generators', function () {
+      this.router.updateAvailableGenerators();
+      assert.ok(!this.router.generators['phoenix-misc'], 'phoenix:misc ignored');
+      assert.ok(!this.router.generators['phoenix-sub-app'], 'phoenix:sub-app ignored');
     });
   });
 });
