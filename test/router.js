e7445c3855236e40755f7193f949cecadb61285e
Mat Garcia <Mat Garcia>
2016-11-23 17:49:55 -0700

    Lint.

--- test/router.js
+++ test/router.js
@@ -7,9 +7,9 @@ var proxyquire = require('proxyquire');
 var path = require('path');
 var Router = proxyquire('../lib/router', {
   'read-pkg-up': {
-    sync: function(options) {
+    sync: function (options) {
       // turn /phoenix/app into phoenix-app
-      var name = options.cwd.split(path.sep).filter(function(chunk) {
+      var name = options.cwd.split(path.sep).filter(function (chunk) {
         return !!chunk;
       }).join('-');
       return {
@@ -22,38 +22,38 @@ var Router = proxyquire('../lib/router', {
   }
 });
 
-describe('Router', function() {
-  beforeEach(function() {
+describe('Router', function () {
+  beforeEach(function () {
     this.env = helpers.fakeEnv();
     this.env.getGeneratorsMeta = sinon.stub();
     this.router = new Router(this.env);
   });
 
-  describe('#registerRoute()', function() {
-    it('is chainable', function() {
+  describe('#registerRoute()', function () {
+    it('is chainable', function () {
       assert.equal(this.router.registerRoute('foo', _.noop), this.router);
     });
   });
 
-  describe('#navigate()', function() {
-    beforeEach(function() {
+  describe('#navigate()', function () {
+    beforeEach(function () {
       this.route = sinon.spy();
       this.router.registerRoute('foo', this.route);
     });
 
-    it('call a route passing router as first argument', function() {
+    it('call a route passing router as first argument', function () {
       this.router.navigate('foo');
       sinon.assert.calledWith(this.route, this.router);
       sinon.assert.calledOnce(this.route);
     });
 
-    it('call a route passing arguments', function() {
+    it('call a route passing arguments', function () {
       this.router.navigate('foo', 'dummy');
       sinon.assert.calledWith(this.route, this.router, 'dummy');
       sinon.assert.calledOnce(this.route);
     });
 
-    it('throws on invalid route name', function() {
+    it('throws on invalid route name', function () {
       assert.throws(this.router.navigate.bind(this.route, 'invalid route name'));
     });
   });
